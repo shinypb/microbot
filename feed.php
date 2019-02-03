6836a23de22f60f1d728fb933849b07e77979f1e
Mark Christian <m@rkchristian.ca>
2019-02-02 21:48:02 -0800

    Fix encoding

--- feed.php
+++ feed.php
@@ -22,6 +22,9 @@ header('content-type: application/rss+xml');
 $title = "@{$username}'s microbot blog";
 $description = microbot_get_user_description(SLACK_API_TOKEN, $username);
 
+header('Content-Type: application/rss+xml; charset=utf-8');
+echo '<?xml version="1.0" encoding="UTF-8" ?>';
+
 ?><rss version="2.0">
     <channel>
         <title><?php emit($title); ?></title>
